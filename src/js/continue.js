843dfa716931bcb49c33ff366115c37c8c6f323e
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-09-19 15:02:26 +0300

    fix

--- src/js/continue.js
+++ src/js/continue.js
@@ -1,13 +1,18 @@
-// При завантаженні сторінки
 window.addEventListener('load', function() {
     const savedState = localStorage.getItem('pageState');
 
     if (savedState === 'second') {
         document.querySelector('.first').style.display = 'none';
         document.querySelector('.second').style.display = 'flex';
+        document.querySelector('.third').style.display = 'none'; // Переконатися, що третя секція не відображається
+    } else if (savedState === 'third') {
+        document.querySelector('.first').style.display = 'none';
+        document.querySelector('.second').style.display = 'none';
+        document.querySelector('.third').style.display = 'flex'; // Показати третю секцію
     } else {
         document.querySelector('.first').style.display = 'flex';
         document.querySelector('.second').style.display = 'none';
+        document.querySelector('.third').style.display = 'none'; // Переконатися, що третя секція не відображається
     }
 });
 
@@ -31,4 +36,15 @@ document.querySelector('.no').addEventListener('click', function() {
 
     // Зберегти стан у localStorage
     localStorage.setItem('pageState', 'first');
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+    // Сховати елемент .second
+    document.querySelector('.second').style.display = 'none';
+
+    // Показати елемент .third
+    document.querySelector('.third').style.display = 'flex';
+
+    // Зберегти стан у localStorage
+    localStorage.setItem('pageState', 'third');
 });

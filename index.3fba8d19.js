e1bd51cb45028740596f3fb9ccd550171ca3b066
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2024-09-19 09:48:37 +0000

    Deploying to gh-pages from @ AlekseyDemydov/game@f972cc35cf818d0508a2d20ab8665a675f335948 🚀

---
+++ index.3fba8d19.js
@@ -0,0 +1,2 @@
+document.getElementById("soundButton").addEventListener("click",(function(){const e=document.getElementById("soundOn"),n=document.getElementById("soundOff");"block"===e.style.display?(e.style.display="none",n.style.display="block"):(e.style.display="block",n.style.display="none")}));
+//# sourceMappingURL=index.3fba8d19.js.map
---
+++ index.b34e591a.js
@@ -0,0 +1,2 @@
+document.getElementById("soundButton").addEventListener("click",(function(){var e=document.getElementById("soundOn"),n=document.getElementById("soundOff");"block"===e.style.display?(e.style.display="none",n.style.display="block"):(e.style.display="block",n.style.display="none")}));
+//# sourceMappingURL=index.b34e591a.js.map
